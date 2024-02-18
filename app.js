1ef54b7eced465df5f33b02758685f5cf1af5671
ikai2 <ikai2@hotmail.com>
2024-02-18 17:16:01 +0200

    fixed accessbility issues

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ acc.forEach(btn => {
         const clickedBtn = e.currentTarget;
         const content = clickedBtn.querySelector('.acc-content');
         const accIcon = clickedBtn.querySelector("img");
-        const isExpanded = content.getAttribute("aria-expanded") === "false"; // this will return true
-        content.setAttribute('aria-expanded', isExpanded); // setting the aria expanded to true to open the accordion
-        accIcon.setAttribute('src', isExpanded ? 'assets/images/icon-minus.svg' : 'assets/images/icon-plus.svg');
+        const isHidden = content.getAttribute("aria-hidden") === "true";
+        const isExpanded = clickedBtn.getAttribute("aria-expanded") === "false"; // this will return true
+        clickedBtn.setAttribute("aria-expanded", isExpanded);
+        content.setAttribute('aria-hidden', !isHidden); // setting the aria expanded to true to open the accordion
+        accIcon.setAttribute('src', isHidden ? 'assets/images/icon-minus.svg' : 'assets/images/icon-plus.svg');
         acc.forEach(otherBtn => {
            if (otherBtn !== clickedBtn) {
-            otherBtn.querySelector('.acc-content').setAttribute("aria-expanded", "false")
+            otherBtn.querySelector('.acc-content').setAttribute("aria-hidden", "true")
+            otherBtn.setAttribute("aria-expanded", "false")
             otherBtn.querySelector('img').setAttribute('src', 'assets/images/icon-plus.svg')
            }            
         })
