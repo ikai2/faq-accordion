19ef7300f96a3da9c7ed371a96654586b041b1b5
ikai2 <ikai2@hotmail.com>
2024-02-18 17:34:11 +0200

    updated the JS logic

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const acc = document.querySelectorAll('.acc');
-acc.forEach(btn => {
-    btn.addEventListener('click', e => {
-        const clickedBtn = e.currentTarget;
-        const content = clickedBtn.querySelector('.acc-content');
-        const accIcon = clickedBtn.querySelector("img");
+const accordions = document.querySelectorAll('.acc');
+accordions.forEach(acc => {
+    const clickedBtn = acc.querySelector(".acc-btn")
+    clickedBtn.addEventListener('click', e => {
+        const content = acc.querySelector('.acc-content');
+        const accIcon = acc.querySelector("img");
         const isHidden = content.getAttribute("aria-hidden") === "true";
-        const isExpanded = clickedBtn.getAttribute("aria-expanded") === "false"; // this will return true
-        clickedBtn.setAttribute("aria-expanded", isExpanded);
+        const isExpanded = acc.getAttribute("aria-expanded") === "false"; // this will return true
+        acc.setAttribute("aria-expanded", isExpanded);
         content.setAttribute('aria-hidden', !isHidden); // setting the aria expanded to true to open the accordion
         accIcon.setAttribute('src', isHidden ? 'assets/images/icon-minus.svg' : 'assets/images/icon-plus.svg');
-        acc.forEach(otherBtn => {
-           if (otherBtn !== clickedBtn) {
+        accordions.forEach(otherBtn => {
+           if (otherBtn !== acc) {
             otherBtn.querySelector('.acc-content').setAttribute("aria-hidden", "true")
             otherBtn.setAttribute("aria-expanded", "false")
             otherBtn.querySelector('img').setAttribute('src', 'assets/images/icon-plus.svg')
